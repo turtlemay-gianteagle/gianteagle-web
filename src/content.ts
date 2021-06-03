0241f014dbebc5a7b120e6f89008066200598be9
Turtlemay <am@trtl.in>
2021-06-03 13:47:34 -0400

    Insert our element on added node.

--- src/content.ts
+++ src/content.ts
@@ -12,6 +12,22 @@ ReactDOM.render(React.createElement(BarcodeWidget), el);
 
 update();
 
+const observer = new MutationObserver((mutations) => {
+	mutations.forEach((mutation) => {
+		mutation.addedNodes.forEach((addedNode) => {
+			if (
+				addedNode instanceof HTMLElement &&
+				addedNode.classList.contains("ProductDetail")
+			) {
+				update();
+			}
+		});
+	});
+}).observe(document.body, {
+	childList: true,
+	subtree: true,
+});
+
 async function update() {
 	const v = await waitForElement(".ProductDetail .mb3");
 	v.insertAdjacentElement("afterend", el);
