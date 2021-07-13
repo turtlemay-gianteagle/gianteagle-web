16e5b3544e29409d9bd1d7930e5ca3f8faba0916
Turtlemay <am@trtl.in>
2021-07-13 13:14:10 -0400

    Extract barcode script to own file.

---
+++ src/barcode.tsx
@@ -0,0 +1,52 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import { BarcodeWidget } from "../components/BarcodeWidget";
+import { waitForElement } from "../lib/util";
+
+render(processItemInfo(document.body.textContent));
+
+const observer = new MutationObserver((mutations) => {
+	mutations.forEach((mutation) => {
+		mutation.addedNodes.forEach((addedNode) => {
+			if (
+				addedNode instanceof HTMLElement &&
+				addedNode.classList.contains("ProductDetail")
+			) {
+				const foundItemInfo = processItemInfo(addedNode.textContent);
+				if (foundItemInfo?.upc) {
+					render(foundItemInfo);
+				}
+			}
+		});
+	});
+}).observe(document.body, {
+	childList: true,
+	subtree: true,
+});
+
+async function render(itemInfo: IItemInfo | null) {
+	const barcodeWidgetRootElem = document.createElement("div");
+	barcodeWidgetRootElem.className = "turtlemay__barcodeWidgetRoot";
+	const v = await waitForElement(".ProductDetail .mb3");
+	v.insertAdjacentElement("afterend", barcodeWidgetRootElem);
+	ReactDOM.render(
+		<BarcodeWidget itemInfo={itemInfo} />,
+		barcodeWidgetRootElem
+	);
+}
+
+function processItemInfo(str: string | null): IItemInfo | null {
+	const foundItemInfo: IItemInfo = {
+		sku: matchSKU(str),
+	};
+	if (foundItemInfo.sku) {
+		foundItemInfo.upc = foundItemInfo.sku.slice(2);
+		return foundItemInfo;
+	} else {
+		return null;
+	}
+}
+
+function matchSKU(str: string | null): string | null {
+	return str?.match(/SKU(\d{14})/)?.[1] || null;
+}
