f400b3199b200623d21d9c3d77bb1c0b616bc400
Turtlemay <am@trtl.in>
2022-01-02 08:18:40 -0500

    Updated selector.

--- src/result.ts
+++ src/result.ts
@@ -6,7 +6,7 @@ addEventListener("keydown", (event: KeyboardEvent) => {
 	if (document.activeElement?.nodeName !== "INPUT") {
 		if (event.key.match(/^([1-9])$/)?.[1]) {
 			const i = Number(event.key) + 1;
-			const allElems = document.querySelectorAll(".ProductsList .ProductTileFull a");
+			const allElems = document.querySelectorAll(".ProductsList .ProductTile a");
 			const clickElem = allElems[i] as HTMLElement | undefined;
 			clickElem?.click();
 		}
