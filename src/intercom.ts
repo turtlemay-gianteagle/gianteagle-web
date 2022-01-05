f909f929792edb017f6abf1b0b6e725f030d6832
Turtlemay <am@trtl.in>
2022-01-05 04:53:42 -0500

    Format for readability.

--- src/intercom.ts
+++ src/intercom.ts
@@ -4,8 +4,17 @@
 
 import { waitForElement } from "../lib/util";
 
-waitForElement("#intercom-frame").then((v) => v.remove());
-waitForElement("#intercom-container").then((v) => v.remove());
-waitForElement("#intercom-css-container").then((v) => v.remove());
-waitForElement(".intercom-lightweight-app").then((v) => v.remove());
-waitForElement(".intercom-app").then((v) => v.remove());
+waitForElement("#intercom-frame")
+	.then((v) => v.remove());
+
+waitForElement("#intercom-container")
+	.then((v) => v.remove());
+
+waitForElement("#intercom-css-container")
+	.then((v) => v.remove());
+
+waitForElement(".intercom-lightweight-app")
+	.then((v) => v.remove());
+
+waitForElement(".intercom-app")
+	.then((v) => v.remove());
