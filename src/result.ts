8bc60d1b62ae88b2e46b30190dca2cdca88fe8e2
Turtlemay <am@trtl.in>
2022-01-05 20:46:58 -0500

    Fixed selecting result by number.

--- src/result.ts
+++ src/result.ts
@@ -5,9 +5,8 @@
 addEventListener("keydown", (event: KeyboardEvent) => {
 	if (document.activeElement?.nodeName !== "INPUT") {
 		if (event.key.match(/^([1-9])$/)?.[1]) {
-			const i = Number(event.key) + 1;
-			const allElems = document.querySelectorAll(".ProductsList .ProductTile a");
-			const clickElem = allElems[i] as HTMLElement | undefined;
+			const allElems = document.querySelectorAll<HTMLElement>(".ProductsList .ProductTile a .ProductImage");
+			const clickElem = allElems[Number(event.key) - 1];
 			clickElem?.click();
 		}
 	}
