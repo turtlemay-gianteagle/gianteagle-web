5b2c40d046655eb43a6b21f7a33437d29cb9fd89
Turtlemay <am@trtl.in>
2022-03-09 02:04:54 -0500

    Output release zip.

--- Foyfile.js
+++ Foyfile.js
@@ -5,5 +5,7 @@ const packageJson = require("./package.json");
 task("build", async (ctx) => {
 	await ctx.exec("webpack --mode=production");
 	await fs.mkdirp("./dist");
-	await ctx.exec(`zip -D -r ./dist/${packageJson.name}-${manifestJson.version}.zip ./css ./out ./manifest.json`);
+	const outZip = `./dist/${packageJson.name}-${manifestJson.version}.zip`;
+	await ctx.exec(`zip -D -r ${outZip} ./css ./out ./manifest.json`);
+	await fs.copy(outZip, `./dist/${packageJson.name}.zip`);
 });
